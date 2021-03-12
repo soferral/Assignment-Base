36bf0ca77b777a8bee51f7ff0fbd624b5cdcaad0
YWANG203 <49288220+YWANG203@users.noreply.github.com>
2021-03-13 02:16:22 +0800

    add new script.js

--- public/script.js
+++ public/script.js
@@ -2,23 +2,66 @@ async function windowActions() {
     console.log('window loaded');
     const form = document.querySelector('.userform');
     const search = document.querySelector('#city');
-    const targetBox = document.querySelector('.targetBox');
+    const suggestions = document.querySelector('.suggestions');  
 
-    const results = await fetch('/api');
+    const results = [];
+    const request = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const data = await request.json();
+   
+    console.log(data);
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        console.log('submit fired', search.value);
-        const filtered = data.filter((record) => record.city.toUpperCase() === search.value.toUpperCase());
-        filtered.forEachItem = document.createElement("li");
-        appendItem.innerText = item.city;
-        targetList.append(appendItem);
-    });
-    
     search.addEventListener('input', (event) => {
         console.log('input', event.target.value);
     });
+    
+    function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+            // here we need to figure out if the city on state matches what was searched
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.city.match(regex) || place.state.match(regex)
+        });
+    }
+
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    function displayMatches(event) {
+        const matchArray = findMatches(event.target.value, data);
+        const html = matchArray.map(place => {
+            const regex = new RegExp(event.target.value, 'gi');
+            const cityName = place.city.replace(regex, '<span class="hl">${event.target.value}</span>')
+            const stateName = place.state.replace(regex, '<span class="hl">${event.target.value}</span>')
+            return `           
+                <div class="box">
+                    <li>
+                        <span class="title">${place.name}</span>
+                    </li>
+                    <li>
+                        <span class="category">${place.category}</span>
+                    </li>
+                    <li>
+                        <span class="address">${place.address_line_1}</span>
+                    </li>
+                    <li>
+                        <span class="location">${place.city}, ${place.state}</span>
+                    </li>
+                    <li>
+                        <span class="zipcode">${place.zip}</span>
+                    </li>
+                </div>           
+            `;
+        }).join('');
+        suggestions.innerHTML = html;
+    
+    }
+
+    
+    search.addEventListener('change', displayMatches);
+    search.addEventListener('keyup', (event) => { 
+        displayMatches(event)
+    });
+
 }
 
 window.onload = windowActions;
